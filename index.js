829b4c990f3a7c449eb7bce6b9fc55f43f62eef5
karishma-fariha <karishmafarihakathi10@gmail.com>
2025-12-18 17:56:13 +0600

    project set up commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+
+app.get('/',(req,res)=>{
+    res.send("Hello,its Blood Donation app")
+});
+app.listen(port,()=>{
+    console.log(`Server is running on ${port}`)
+});
