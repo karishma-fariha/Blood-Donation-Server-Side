ae1f3018f3f9c83a5e693ca38606eaafb20018f8
karishma-fariha <karishmafarihakathi10@gmail.com>
2025-12-20 21:58:20 +0600

    addd single donation rqst to fill the update form

--- index.js
+++ index.js
@@ -44,34 +44,67 @@ async function run() {
     })
 
     //create donation requests
-    app.post("/donation-requests",async(req,res)=>{
+    app.post("/donation-requests", async (req, res) => {
       const requestData = req.body;
-      const user = await userCollection.findOne({email:requestData.requesterEmail})
+      const user = await userCollection.findOne({ email: requestData.requesterEmail })
       if (user?.status === 'blocked') {
         return res.status(403).send({ message: "Blocked users cannot create requests." });
-    }
-    const newRequest = {
+      }
+      const newRequest = {
         ...requestData,
         status: "pending",
         createdAt: new Date()
-    };
+      };
       const result = await donationCollection.insertOne(newRequest);
       res.send(result)
-    })  
+    })
 
-// get a single donation request
+    // get a single donation request
     app.get("/donation-requests/recent/:email", async (req, res) => {
-    const email = req.params.email;
-    const query = { requesterEmail: email };
-    
-    
-    const result = await donationCollection.find(query)
+      const email = req.params.email;
+      const query = { requesterEmail: email };
+
+
+      const result = await donationCollection.find(query)
         .sort({ createdAt: -1 })
         .limit(3)
         .toArray();
-        
-    res.send(result);
-});
+
+      res.send(result);
+    });
+
+    //  set query for pagination
+    app.get("/donation-requests/my-requests/:email", async (req, res) => {
+      const email = req.params.email;
+      const filterStatus = req.query.status;
+      const page = parseInt(req.query.page) || 0;
+      const size = parseInt(req.query.size) || 5;
+
+      let query = { requesterEmail: email };
+
+      if (filterStatus && filterStatus !== 'all') {
+        query.status = filterStatus;
+      }
+
+      const result = await donationCollection.find(query)
+        .sort({ createdAt: -1 })
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+
+      const count = await donationCollection.countDocuments(query);
+
+      res.send({ result, count });
+    });
+
+
+    //Get a single donation request to fill the update form
+    app.get("/donation-request/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await donationCollection.findOne(query);
+      res.send(result);
+    });
 
     // get a single user
     app.get('/users/:email', async (req, res) => {
