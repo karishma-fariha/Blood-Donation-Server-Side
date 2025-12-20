dcd567459860f7bffdae129ec83663a5da8b9cfb
karishma-fariha <karishmafarihakathi10@gmail.com>
2025-12-20 12:08:53 +0600

    added post api

--- index.js
+++ index.js
@@ -23,8 +23,21 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
+
+    const database = client.db('blooddonationdb')
+    const userCollection = database.collection('user')
+
+
+    app.post('/users',async(req,res)=>{
+        const userInfo = req.body; 
+        userInfo.role ="Donor";
+        userInfo.createdAt = new Date();
+
+        const result = await userCollection.insertOne(userInfo);
+        res.send(result)
+    })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
